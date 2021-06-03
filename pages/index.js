c80823f3147cafe8b8ebba3d2b21dd7c9bc54580
Robert Fish <elasys63+github@gmail.com>
2021-06-04 01:39:46 +1000

    added tailwind jit

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,11 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <div className="container mx-auto px-4">
+      <button className="bg-green-500 text-white hover:bg-white hover:text-green-500 p-2 rounded-md border-green-500 border">
+        click me
+      </button>
+    </div>
+  );
 }
