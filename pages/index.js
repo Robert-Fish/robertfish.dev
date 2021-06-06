c0e086ee23fc8e930b564b9f1898856773f6254f
Robert Fish <elasys63+github@gmail.com>
2021-06-06 22:03:28 +1000

    reverted storybook

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="container mx-auto px-4">
+    <div>
       <button className="bg-green-500 text-white hover:bg-white hover:text-green-500 p-2 rounded-md border-green-500 border">
         click me
       </button>
