e7c9fb5677e96dd8c06e3f79acb9a091bb2ec1b1
Robert Fish <elasys63+github@gmail.com>
2021-06-06 22:26:35 +1000

    added some basic content to homepage

--- components/Container.js
+++ components/Container.js
@@ -5,9 +5,17 @@ export default function Container({ children }) {
   return (
     <div className="bg-white dark:bg-gray-800">
       <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-gray-800 bg-opacity-60 dark:text-gray-100">
-        <div>
+        <div className="w-full">
+          <NextLink href="/">
+            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Home</a>
+          </NextLink>
           <NextLink href="/blog">
             <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">Blog</a>
+          </NextLink>
+          <NextLink href="/blog">
+            <a className="p-1 text-gray-900 sm:p-4 dark:text-gray-100">
+              Newsletter
+            </a>
           </NextLink>
         </div>
       </nav>
--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,22 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <Container>hello</Container>;
+  return (
+    <Container>
+      <div className="flex flex-col justify-center items-start  w-1/2 mx-auto mb-16 ">
+        <div className="flex flex-row justify-evenly w-full items-center">
+          <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
+            Hey, I’m Robert Fish
+          </h1>
+          <Image
+            alt="Lee Robinson"
+            height={130}
+            width={130}
+            src="/avatar.jpeg"
+            className="rounded-full"
+          />
+        </div>
+      </div>
+    </Container>
+  );
 }
